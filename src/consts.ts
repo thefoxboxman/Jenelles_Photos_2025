3d361f688892fe6bbcca0063defe771b562ba659
John De Costa <thefoxboxman@users.noreply.github.com>
2025-01-07 15:26:24 +1000

    major lay changes and extra posts for testing

--- src/consts.ts
+++ src/consts.ts
@@ -30,7 +30,7 @@ export function getCategoryMetadata(category: string) {
 
 // Header Links, src/components/Header.astro
 export const HeaderLinks = [
-   { href: "/category/One/1/", title: "Photos" },
+   { href: "/category/Photos/Photos/", title: "Photos" },
   // { href: "/category/Two/1/", title: "Two" },
   // { href: "/category/Three/1/", title: "Three" },
 ];
--- src/content/posts/post-3.md
+++ src/content/posts/post-3.md
@@ -1,12 +1,12 @@
 ---
 title: Pair of Brown Doves
-slug: post_1
+slug: post_3
 description: Highfield Falls Park
 category:
   - Birds
 tags:
   - Birds
-pubDate: 2025-01-01
+pubDate: 2025-01-03
 cover: https://github.com/thefoxboxman/Jenelles-Image-Store-2025/blob/main/Post-1_DSC9319-Edit.jpg?raw=true
 coverAlt: Pair of Brown Doves
 author: John
